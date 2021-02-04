d485d0f4e6d71798b61efc23d2d1cfbfc79770af
Doireann Herold <ivydoor@icloud.com>
2021-02-04 14:56:44 -0800

    Profile page routed

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
  path('', views.home, name='home'),
  path('about', views.about, name= 'about'),
-#  path('profile', views.profile, name= 'profile'),
+ path('profile', views.profile, name= 'profile'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
-
-# def profile(request):
-#   return render(request, 'profile.html')
+  
+def profile(request):
+  return render(request, 'profile.html')
