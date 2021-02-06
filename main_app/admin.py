dae2d2db7a52a04b23ed5e2c25fb9c0f8e3c7d4a
Doireann Herold <ivydoor@icloud.com>
2021-02-05 20:21:39 -0800

    slight css changes and Question added to Models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Memory
+from .models import Memory, Question
 from django.contrib.auth.admin import UserAdmin
 
 UserAdmin.list_display = ('username', 'email', 'is_active', 'date_joined', 'is_staff')
 
 admin.site.register(Memory)
+admin.site.register(Question)
