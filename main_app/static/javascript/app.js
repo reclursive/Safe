ccbbc771f5332df367214cd100d48d64eca8c72f
Doireann Herold <ivydoor@icloud.com>
2021-02-10 10:19:01 -0800

    Jquery and modal attempts

--- main_app/static/javascript/app.js
+++ main_app/static/javascript/app.js
@@ -1,1 +1,8 @@
-const testQs = $('.form-check-input1 input:radio:checked').val()
+// const testQs = $('#flexRadioDefault3 input:radio:checked').val()
+
+// const testQs = $('#test-submit').click(function() {
+//   if($('#lexRadioDefault3').is(':checked')) { $(location).attr('href', http://localhost:8000/90/question2; }
+    //  else { $(location).attr('href', 'profile'); }
+// });
+
+
