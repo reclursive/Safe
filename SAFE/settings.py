0f31402d803aac3d27feb3cd851bfecd07129e31
Doireann Herold <ivydoor@icloud.com>
2021-02-16 17:22:49 -0800

    testing DIRS, testing again

--- SAFE/settings.py
+++ SAFE/settings.py
@@ -61,6 +61,7 @@ ROOT_URLCONF = 'SAFE.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
         'APP_DIRS': True,
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
